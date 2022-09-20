feea8b72020888221439b279621adf4e7bfe390e
Aleksandr-III <110060153+Aleksandr-III@users.noreply.github.com>
2022-09-21 02:39:27 +0500

    Update Program.cs
    solving

--- Program.cs
+++ Program.cs
@@ -4,3 +4,13 @@ int size = Convert.ToInt32(Console.ReadLine());
 string[] arrayStrings = new string[size];
 int limit = 3;
 int position = 0;
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i + 1}-й элемент: ");
+    string element = Convert.ToString(Console.ReadLine());
+    if (element.Length <= limit)
+    {
+        arrayStrings[position] = element;
+        position++;
+    }
+}
